13c62b88ca7d009da3f46bd7b5d4134e0ecc2a33
altscherz <xushulamit@gmail.com>
2024-06-08 18:29:04 +0800

    默认

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view />
+    <footer>
+      <router-link to="/about">© 2024 Altscherz</router-link>
+    </footer>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
-  padding: 30px;
+  padding: 1em;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  margin: 0 1em;
+  text-decoration: none;
+  color: #42b983;
 }
 
 nav a.router-link-exact-active {
+  font-weight: bold;
+}
+
+footer {
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: 1em 0;
+  background-color: #f8f9fa;
+}
+
+footer a {
+  text-decoration: none;
   color: #42b983;
 }
 </style>
